50de359ca9cb6ded5bac5e408254ffd49402d05b
Carter Hughes <carter.hughes@montgomeryparks.org>
2024-04-16 16:08:41 -0400

    Update with IGNORED_FIELDS logic and layers to loop through

--- helpers/FeatureService_Azure_Helper.py
+++ helpers/FeatureService_Azure_Helper.py
@@ -9,8 +9,15 @@ Purpose: Help draft parameters, T-SQL, etc. for querying data originating from a
 
 from arcgis.gis import GIS
 from arcgis.features import FeatureLayer
-gis = GIS("home")
+gis = GIS(profile="work")
 
+
+# In[48]:
+IGNORED_FIELDS = [
+    'X',
+    'Y',
+    'ASSET_TYPE'
+]
 
 # In[49]:
 
@@ -30,7 +37,7 @@ def get_featurelayer_field_names(
     featurelayer_url : str,
     gis : GIS
 ) -> str:
-    return ', '.join([field['name'] for field in get_featurelayer_fields(featurelayer_url=featurelayer_url, gis=gis)])
+    return ', '.join([field['name'] for field in get_featurelayer_fields(featurelayer_url=featurelayer_url, gis=gis) if field['name'] not in IGNORED_FIELDS])
 
 def get_featurelayer_bronzesqlfields(
     featurelayer_url : str,
@@ -48,11 +55,13 @@ def get_featurelayer_bronzesqlfields(
         'esriFieldTypeDate': lambda field: 'INT',
         'esriFieldTypeDouble' : lambda field: "NUMERIC(38, 8)",
         'esriFieldTypeFloat' : lambda field: "NUMERIC(12, 6)",
+        'esriFieldTypeSingle' : lambda field: "NUMERIC(12, 6)",
         'esriFieldTypeBlob': lambda field: 'BINARY'
     }
     sql = ''''''
     for field in fields:
-        sql += field['name'] + ' ' + type_mappings[field['type']](field) + ''',
+        if field['name'] not in IGNORED_FIELDS:
+            sql += field['name'] + ' ' + type_mappings[field['type']](field) + ''',
 '''
     sql = sql[:-2]
     return sql
@@ -60,5 +69,18 @@ def get_featurelayer_bronzesqlfields(
 
 # In[50]:
 
+layers = [
+    ('Benches', 'https://services1.arcgis.com/HbzrdBZjOwNHp70P/arcgis/rest/services/Montgomery_Parks_Benches/FeatureServer/0'), # 19ce5bf022dc4193aeac8b18833e094f
+    ('BikeAssets', 'https://services1.arcgis.com/HbzrdBZjOwNHp70P/arcgis/rest/services/Bike_Assets/FeatureServer/0'), # 46de90ff5980452b9beffe37d377252a
+    ('Kiosks', 'https://services1.arcgis.com/HbzrdBZjOwNHp70P/arcgis/rest/services/Kiosks_and_Signs/FeatureServer/1'), # d4a8d962a98a4944bfd665fe8bea2fcb
+    ('SignLocations', 'https://services1.arcgis.com/HbzrdBZjOwNHp70P/arcgis/rest/services/Kiosks_and_Signs/FeatureServer/0'), # d4a8d962a98a4944bfd665fe8bea2fcb
+    ('AssetsOther', 'https://montgomeryplans.org/server/rest/services/Parks/Assets_Pt/FeatureServer/0')
+]
 
-print(get_featurelayer_bronzesqlfields('https://services1.arcgis.com/HbzrdBZjOwNHp70P/arcgis/rest/services/ParkBuildings/FeatureServer/0', gis=gis))
+for layer in layers:
+    print(layer[0])
+    print('')
+    print(get_featurelayer_field_names(layer[1], gis=gis))
+    print('')
+    print(get_featurelayer_bronzesqlfields(layer[1], gis=gis))
+    print('')
